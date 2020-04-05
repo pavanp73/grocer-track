9d6d54ce3cd88f09dc3227a1f35f7a2f4a305d11
Pavan Padmanabhi <pavan.padmanabhi@gmail.com>
2020-04-05 17:49:05 +0530

    Initial setup

---
+++ src/main/java/com/pavan/track/GrocerTrackApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocerTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrocerTrackApplication.class, args);
+	}
+
+}
